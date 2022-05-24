bdb8e9cd0e45a0bfafbb201b7217eb884dd71b53
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-24 22:07:31 +0200

    mod

--- snacks 2/script.js
+++ snacks 2/script.js
@@ -1,2 +1,19 @@
 
-//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+//Generatore di “nomi cognomi” casuali: il Grande Gatsby ha 
+// una lista di nomi e una lista di cognomi, e da queste vuole 
+//generare una falsa lista di invitati con nome e cognome.
+
+const listaNomiRandom = ["Mario" , "Giulio" , "Antonio" , "Giuseppe", "Domenico"];
+const listaCognomiRandom = [ "Rossi", "Verdi", "Gialli", "Bianchi", "Marroni"];
+console.log(listaNomiRandom,listaCognomiRandom);
+
+
+for (let i = 0; i < listaNomiRandom.length; i++) {
+    console.log(listaNomiRandom[i]);
+
+}
+
+for (let i = 0; i < listaCognomiRandom.length; i++) {
+    console.log(listaCognomiRandom[i]);
+
+}
