c3eb5170ef55d156e3c16d7c489c2c0cbe24a396
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-24 22:18:54 +0200

    mod

--- snacks 1/script.js
+++ snacks 1/script.js
@@ -1,1 +1,16 @@
 //Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+let listaNumeri = ["1","2","3","4","5","6","7","8","9","10"];
+
+for(let i=0; i<10; i++){
+    listaNumeri = parseInt(prompt('Inserisci un numero'));
+
+    if (listaNumeri % 2 === 0){
+        console.log(listaNumeri);
+
+    }else(listaNumeri % 2 === 1 )
+        console.log ()
+
+    
+    
+}
