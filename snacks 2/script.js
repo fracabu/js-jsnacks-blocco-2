b62ef88e0c0c8c7638ee1059dcb38107abc68d67
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-25 13:46:46 +0200

    correzine snacks2

--- snacks 2/script.js
+++ snacks 2/script.js
@@ -3,17 +3,16 @@
 // una lista di nomi e una lista di cognomi, e da queste vuole 
 //generare una falsa lista di invitati con nome e cognome.
 
-const listaNomiRandom = ["Mario" , "Giulio" , "Antonio" , "Giuseppe", "Domenico"];
-const listaCognomiRandom = [ "Rossi", "Verdi", "Gialli", "Bianchi", "Marroni"];
-console.log(listaNomiRandom,listaCognomiRandom);
+let Nomi = ["Mario" , "Giulio" , "Antonio" , "Giuseppe", "Domenico",];
+let Cognomi = [ "Rossi", "Verdi", "Gialli", "Bianchi", "Marroni",];
+let RandomNomiCognomi = [];
 
 
-for (let i = 0; i < listaNomiRandom.length; i++) {
-    console.log(listaNomiRandom[i]);
-
-}
-
-for (let i = 0; i < listaCognomiRandom.length; i++) {
-    console.log(listaCognomiRandom[i]);
+for (let i = 1; i <=5; i++) {
+    let num = Math.floor(Math.random() * 5);
+    let numSecond =  Math.floor(Math.random() * 5);
+    RandomNomiCognomi.push ([Nomi[num] + "" + Cognomi[numSecond]]);
 
 }
+
+console.log(RandomNomiCognomi);
